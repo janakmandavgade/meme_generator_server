2da5e3f3175ee0bc2d345a911ef713b71da2b491
Janak Mandavgade <janak2703@gmail.com>
2025-07-12 20:38:41 +0530

    added in /health api route for server to be on always

--- app.py
+++ app.py
@@ -347,6 +347,11 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
+@app.get("/health")
+async def health():
+    print("Healthy app!")
+    return {"status": "healthy"}
+
 @app.post("/start")
 async def start(payload: StartInput):
     print("Payload is: ",payload)
